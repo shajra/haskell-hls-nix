4c89311060c1dcc4f9649f11e089c0927a0d1cf0
Sukant Hajra <rrudbskr5g@snkmail.com>
2020-10-05 03:30:42 -0500

    update dependencies

--- config.nix
+++ config.nix
@@ -3,8 +3,8 @@
     haskell-nix.useMaterialization = false;
     haskell-nix.checkMaterialization = false;
     haskell-nix.hackage.index = {
-        state = "2020-09-27T00:00:00Z";
-        sha256 = "0f1llfnmbsd5x1c45imc7sx0hj03j6j3g1a23m9b6f74barx4pw2";
+        state = "2020-10-05T00:00:00Z";
+        sha256 = "1sazxknvajhyb6v8xhmi0ajgnz5jz300iww75gh5x8xnaf7lhfvw";
     };
     haskell-nix.nixpkgs-pin = "nixpkgs-2003";
     haskell-nix.plan = {
