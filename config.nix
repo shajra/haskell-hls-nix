83ad38a56fec4760862af15117425d58b0e13b4f
Sukant Hajra <dev.sukant@hajra.xyz>
2021-02-27 07:18:10 -0600

    removed unneeded lookup SHAs

--- config.nix
+++ config.nix
@@ -9,14 +9,4 @@
         sha256 = "f8ef4679f4c3f0c7a075dfef715f1db1b51e3934dc28cef9b3f83b2e1f1e2f77";
     };
     haskell-nix.nixpkgs-pin = "nixpkgs-2009";
-
-    haskell-nix.lookupSha256.unstable = {
-        "https://github.com/alanz/ghc-exactprint.git" =
-            "18r41290xnlizgdwkvz16s7v8k2znc7h215sb1snw6ga8lbv60rb";
-    };
-
-    haskell-nix.lookupSha256.released = {
-        "https://github.com/alanz/ghc-exactprint.git" =
-            "18r41290xnlizgdwkvz16s7v8k2znc7h215sb1snw6ga8lbv60rb";
-    };
 }
